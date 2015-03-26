3dc2a3fd849c3476ef2b57266f12da145026402c
unixuser <chrisskeddy@gmail.com>
2015-03-26 14:02:59 -0400

    Update uu-conkyrc.sh

--- uu-conkyrc.sh
+++ uu-conkyrc.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-time=`date +%x-%H:%M:%S`
-bak=".bak-$time"
+dtime=`date +%x-%H:%M:%S`
+bak=".bak-$dtime"
 
 if [ -e $HOME/.conkyrc ] ; then
   mv $HOME/.conkyrc $HOME/.conkyrc-$bak
 fi
+echo -n "HD/SSD name: "
+read drive
